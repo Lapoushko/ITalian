d234312ee1cee16c0ef9fb9055e5899ce6c37a67
CollRed <mylnikov2003@bk.ru>
2022-04-14 21:19:47 +0500

    Создал врага

---
+++ Assets/Scripts/Enity.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Enity : MonoBehaviour
+{
+    public virtual void GetDamage()
+    {
+        
+    }
+
+    
+    public virtual void Die()
+    {
+
+        
+    }
+}
---
+++ Assets/Scripts/Virus.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Virus : MonoBehaviour
+{
+    public float speed = 3.5f;
+    private Vector3 dir;
+    private SpriteRenderer sprite;
+
+    private new void Start()
+    {
+        dir = transform.right;   
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + transform.up * 0.1f + transform.right * dir.x * 0.7f, 0.1f);
+
+        if (colliders.Length > 0) dir *= -1f;
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, Time.deltaTime);
+    }
+}
